60e26cc88944f37f68158bb416014a8ee82b5b45
Ethen-007 <ethenbiju@gmail.com>
2024-04-14 21:53:08 +0530

    learned input output and conditionals

---
+++ src/input_output.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class input_output{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of students : ");
+        int n = sc.nextInt();
+        System.out.println(n);
+    }
+}
