6ec4e504b24ce0b3c207453dc27f66c65d5b41ba
Ethen-007 <ethenbiju@gmail.com>
2024-04-15 22:51:33 +0530

    learned conditionals,functions and methods

---
+++ src/Conditions.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Conditions {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+        if(a>b){
+            System.out.println("a is grt than b");
+        }else if(a<b){
+            System.out.println("a is less than b");
+        }else{
+            System.out.println("they are equal");
+        }
+    }
+}
