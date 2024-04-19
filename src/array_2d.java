e2a663d032fc328b2daa1ad5c8cd5aa166aff1fa
Ethen-007 <ethenbiju@gmail.com>
2024-04-19 23:49:11 +0530

    2D array in java

---
+++ src/array_2d.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class array_2d {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size number of rows");
+        int rows=sc.nextInt();
+        System.out.println("Enter the size number of columns");
+        int cols=sc.nextInt();
+        int[][] numbers = new int[rows][cols];
+        //input
+        //rows
+        for(int i=0;i<rows;i++){
+            //columns
+            for(int j=0;j<cols;j++){
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+        //output
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                System.out.print(numbers[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
