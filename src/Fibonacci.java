0f523a5f34a5807c6e182d2f17d1962827f3f876
Ethen-007 <ethenbiju@gmail.com>
2024-05-01 21:29:12 +0530

    fibo series in java

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Fibonacci {
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void fibo(int a,int b,int n) {
+        if(n==0){
+            return; //break case
+        }
+        int c =a+b;
+        System.out.print( c+" ");
+        n--;
+        fibo(b,c,n);
+    }
+    public static void main(String[] args){
+        System.out.println("Enter the number of terms:");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a =0, b =1;
+        System.out.print(a + " " + b+" ");
+        fibo(a,b,n-2);
+    }
 }
