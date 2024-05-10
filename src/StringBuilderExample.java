427adfac97c32ecc4f9a8270d14626fc2ba0af2c
Ethen-007 <ethenbiju@gmail.com>
2024-05-10 23:26:45 +0530

    string builder in java

--- src/StringBuilderExample.java
+++ src/StringBuilderExample.java
@@ -1,10 +1,13 @@
 
-public class StringBuilder {
+public  class StringBuilderExample {
     public static void main(String[] args) {
-        StringBuilder sb=new StringBuilder();
+        StringBuilder sb=new StringBuilder("Tony");
+        sb.append("hello");
+        System.out.println(sb);
     }
 }
-setCharAt;
-sb.insert(2,'n');
-sb.delete(2,3); 3 is inconclusive
-sb.append('h');
+//setCharAt;
+//sb.insert(2,'n');
+//sb.delete(2,3); 3 is inconclusive
+//sb.append('h');
+//sb.length();
