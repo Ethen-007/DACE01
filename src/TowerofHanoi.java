52004e1df41927172df7a38db52e22733d15fd2a
Ethen-007 <ethenbiju@gmail.com>
2024-05-27 23:58:09 +0530

    modified tower of hanoi

--- src/TowerofHanoi.java
+++ src/TowerofHanoi.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
 public class TowerofHanoi {
-    public static void towerofhanoi(int n,String src,String helper,String dest){
+    public static void towerofhanoi(int n,String from_rod,String to_rod,String helper_rod){
         if(n==1){
-            System.out.println("transfer disk "+ n + " from "+ src+ " to "+ dest);
+            System.out.println("transfer disk "+ n + " from "+ from_rod+ " to "+ to_rod);
             return;
         }
-        towerofhanoi(n-1,src,dest,helper);
-        System.out.println("transfer disk "+ n + " from "+ src+ " to "+ dest);
-        towerofhanoi(n-1,helper,dest,src);
+        towerofhanoi(n-1,from_rod,helper_rod,to_rod);
+        System.out.println("transfer disk "+ n + " from "+ from_rod+ " to "+ to_rod);
+        towerofhanoi(n-1,helper_rod,to_rod,from_rod);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
          System.out.println("Enter the number of disks");
         int n = sc.nextInt();
-        towerofhanoi(n,"S","H","D");
+        towerofhanoi(n,"S","D","H");
     }
 }
