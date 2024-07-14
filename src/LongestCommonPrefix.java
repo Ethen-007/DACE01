115b23eea32a3fd723c4d1275621d4426b7c5f5d
Ethen Biju <ethenbiju@gmail.com>
2024-07-15 00:16:20 +0530

    Create LongestCommonPrefix.java

---
+++ src/LongestCommonPrefix.java
@@ -0,0 +1,13 @@
+class LongestCommonPrefix{
+  public String longestCommonPrefix(String[] str){
+    if(str==null || str.length == 0) return "";
+    String prefix = str[0];
+    for(int i=1;i<str.length;i++){
+      while(str[i].indexOf(prefix)!=0){
+        prefix = prefix.substring(0,i);
+        if(prefix.isEmpty())return "";
+      }
+      return prefix;
+    }
+  }
+}
